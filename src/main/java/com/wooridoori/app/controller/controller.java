a3f0d1cd4e8c2f773b4d01caf9722e55914ab797
Kchiho <kim@chihoui-MacBookAir.local>
2022-09-27 14:06:29 +0900

    test

---
+++ src/main/java/com/wooridoori/app/controller/controller.java
@@ -0,0 +1,14 @@
+package com.wooridoori.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class controller {
+
+	@RequestMapping(value="/login.do",method=RequestMethod.GET)
+	public String selectOneMember() {	
+			return "music.jsp";
+	}
+}
